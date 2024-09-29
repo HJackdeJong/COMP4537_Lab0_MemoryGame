2bb8334370719298f82001a00b7f9f778bcec805
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 16:19:55 -0700

    Lab3: Added getDate function to utils.js and path to it in server.js

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -5,27 +5,19 @@ const { getDate } = require('./modules/utils.js');  // Adjusted path to match pr
 http.createServer(function(req, res) {
     // Construct the URL
     const url = new URL(req.url, `http://${req.headers.host}`);
+    console.log(`Received pathname: ${url.pathname}`);
 
-    // Check if the request is to /getDate
-    if (url.pathname === '/COMP4537/labs/3/getDate') {
-        // Get the name from the query parameters
+    if (url.pathname === '/getDate') {
         const name = url.searchParams.get('name') || "Guest";
-
-        // Get the current date and time
         const currentDate = getDate();
-
-        // Construct the message
+    
         const message = locals.MESSAGES.message
             .replace("%1", name)
             .concat(currentDate);
-
-        // Construct the response headers
+    
         res.writeHead(200, { "Content-Type": "text/html" });
-
-        // Send the response in blue text
         res.end(`<div style="color:blue">${message}</div>`);
     } else {
-        // Handle 404 for other routes
         res.writeHead(404, { "Content-Type": "text/html" });
         res.end("404 Not Found");
     }
