8940f93f7157274659c28e440bcdd69135801c98
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 16:29:19 -0700

    Testing hosting

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -1,7 +1,49 @@
-const http = require('http');  // Use http since Vercel handles HTTPS for you
+const http = require('http');  
+const fs = require('fs');
+const path = require('path');
 const locals = require('./locals/en/en.js');  // Adjusted path to match project structure
 const { getDate } = require('./modules/utils.js');  // Adjusted path to match project structure
 
+// Function to serve static files from a given directory
+function serveStaticFiles(filePath, res) {
+    const fullPath = path.join(__dirname, filePath);
+
+    fs.readFile(fullPath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.end("404 Not Found");
+            } else {
+                res.writeHead(500, { "Content-Type": "text/html" });
+                res.end("500 Internal Server Error");
+            }
+        } else {
+            let ext = path.extname(fullPath).toLowerCase();
+            let contentType = 'text/html';  // Default content type
+
+            // Set appropriate content type for different file extensions
+            const mimeTypes = {
+                '.html': 'text/html',
+                '.js': 'text/javascript',
+                '.css': 'text/css',
+                '.json': 'application/json',
+                '.png': 'image/png',
+                '.jpg': 'image/jpeg',
+                '.gif': 'image/gif',
+                '.svg': 'image/svg+xml',
+            };
+
+            if (mimeTypes[ext]) {
+                contentType = mimeTypes[ext];
+            }
+
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(data);
+        }
+    });
+}
+
+// Create the HTTP server
 http.createServer(function(req, res) {
     // Construct the URL
     const url = new URL(req.url, `http://${req.headers.host}`);
@@ -17,9 +59,12 @@ http.createServer(function(req, res) {
     
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(`<div style="color:blue">${message}</div>`);
+    } else if (url.pathname === '/') {
+        // Serve index.html or other static files when the path is '/'
+        serveStaticFiles('/index.html', res);
     } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("404 Not Found");
+        // Attempt to serve any static file requested
+        serveStaticFiles(url.pathname, res);
     }
 
 }).listen(process.env.PORT || 3000);
