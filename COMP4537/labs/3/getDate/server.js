d6a52ab901ecfa0acc2d10b13e39e7ef3ca5fb35
Harrison de Jong <harrisondijon@gmail.com>
2024-09-29 17:08:42 -0700

    Lab 3 trying other deployment settings

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -1,13 +1,8 @@
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const locals = require('./locals/en/en.js'); // Adjust the path as per your folder structure
+const { getDate } = require('./modules/utils.js'); // Adjust the path as per your folder structure
 
-// Importing the locals and the getDate function from your modules
-const locals = require('./locals/en/en.js');
-const { getDate } = require('./modules/utils.js');
-
-// Create the HTTP server
-http.createServer(function(req, res) {
+const server = (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     console.log(`Received pathname: ${url.pathname}`);
 
@@ -25,7 +20,7 @@ http.createServer(function(req, res) {
         res.writeHead(404, { "Content-Type": "text/html" });
         res.end("404 Not Found");
     }
+};
 
-}).listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
+// Instead of `listen`, we export the handler for Vercel
+module.exports = server;
